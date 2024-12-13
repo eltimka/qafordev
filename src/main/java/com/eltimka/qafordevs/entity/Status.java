9380d1be2fdb988911e8a1fcc96224800ef2703a
Elvira Sumarokava <40741867+eltimka@users.noreply.github.com>
2024-12-13 14:13:21 -0500

    The basic structure of the project has been created

---
+++ src/main/java/com/eltimka/qafordevs/entity/Status.java
@@ -0,0 +1,5 @@
+package com.eltimka.qafordevs.entity;
+
+public enum Status {
+    ACTIVE, DELETED
+}
---
+++ src/main/java/com/eltimka/qafordevs/repository/DeveloperRepository.java
@@ -0,0 +1,16 @@
+package com.eltimka.qafordevs.repository;
+
+import com.eltimka.qafordevs.entity.DeveloperEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface DeveloperRepository extends JpaRepository<DeveloperEntity,Integer> {
+DeveloperEntity findByEmail(String email);
+
+@Query("SELECT d FROM DeveloperEntity d WHERE d.status = 'ACTIVE' AND d.specialty = ?1")
+List<DeveloperEntity> findAllActiveBySpecialty(String specialty);
+
+
+}
