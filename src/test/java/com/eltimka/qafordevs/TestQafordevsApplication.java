829ad044e917b255848671610e3b8c9c8fcadec4
Elvira Sumarokava <40741867+eltimka@users.noreply.github.com>
2024-12-13 10:17:16 -0500

    Project setup

---
+++ src/test/java/com/eltimka/qafordevs/TestQafordevsApplication.java
@@ -0,0 +1,11 @@
+package com.eltimka.qafordevs;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestQafordevsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(QafordevsApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
