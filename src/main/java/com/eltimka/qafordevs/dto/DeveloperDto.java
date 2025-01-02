e1b5e0600250fdcf927722e781070533ae099341
Elvira Sumarokava <40741867+eltimka@users.noreply.github.com>
2025-01-02 09:54:00 -0500

    Added service layer with application business logic, and unit tests for the service layer using Mockito

---
+++ src/main/java/com/eltimka/qafordevs/dto/DeveloperDto.java
@@ -0,0 +1,43 @@
+package com.eltimka.qafordevs.dto;
+
+import com.eltimka.qafordevs.entity.DeveloperEntity;
+import com.eltimka.qafordevs.entity.Status;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@NoArgsConstructor
+@AllArgsConstructor
+public class DeveloperDto {
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String specialty;
+    private Status status;
+
+    public DeveloperEntity toEntity() {
+        return DeveloperEntity.builder()
+                .id(id)
+                .firstName(firstName)
+                .lastName(lastName)
+                .specialty(specialty)
+                .status(status)
+                .build();
+    }
+
+    public static DeveloperDto fromEntity(DeveloperEntity developer) {
+        return DeveloperDto.builder()
+                .id(developer.getId())
+                .firstName(developer.getFirstName())
+                .lastName(developer.getLastName())
+                .specialty(developer.getSpecialty())
+                .status(developer.getStatus())
+                .build();
+    }
+}
--- src/test/java/com/eltimka/qafordevs/util/DataUtils.java
+++ src/test/java/com/eltimka/qafordevs/util/DataUtils.java
@@ -1,5 +1,6 @@
 package com.eltimka.qafordevs.util;
 
+import com.eltimka.qafordevs.dto.DeveloperDto;
 import com.eltimka.qafordevs.entity.DeveloperEntity;
 import com.eltimka.qafordevs.entity.Status;
 
@@ -60,6 +61,69 @@ public class DataUtils {
                 .firstName("Frank")
                 .lastName("Jones")
                 .email("frank.doe@mail.com")
+                .specialty("Java")
+                .status(Status.DELETED)
+                .build();
+    }
+
+    public static DeveloperDto getJohnDoeDtoTransient() {
+        return DeveloperDto.builder()
+                .firstName("John")
+                .lastName("Doe")
+                .email("john.doe@mail.com")
+                .specialty("Java")
+                .status(Status.ACTIVE)
+                .build();
+    }
+
+    public static DeveloperDto getMikeSmithDtoTransient() {
+        return DeveloperDto.builder()
+                .firstName("Mike")
+                .lastName("Smith")
+                .email("mike.smith@mail.com")
+                .specialty("Java")
+                .status(Status.ACTIVE)
+                .build();
+    }
+
+    public static DeveloperDto getFrankJonesDtoTransient() {
+        return DeveloperDto.builder()
+                .firstName("Frank")
+                .lastName("Jones")
+                .email("frank.jones@mail.com")
+                .specialty("Java")
+                .status(Status.DELETED)
+                .build();
+    }
+
+    public static DeveloperDto getJohnDoeDtoPersisted() {
+        return DeveloperDto.builder()
+                .id(1)
+                .firstName("John")
+                .lastName("Doe")
+                .email("john.doe@mail.com")
+                .specialty("Java")
+                .status(Status.ACTIVE)
+                .build();
+    }
+
+    public static DeveloperDto getMikeSmithDtoPersisted() {
+        return DeveloperDto.builder()
+                .id(2)
+                .firstName("Mike")
+                .lastName("Smith")
+                .email("mike.smith@mail.com")
+                .specialty("Java")
+                .status(Status.ACTIVE)
+                .build();
+    }
+
+    public static DeveloperDto getFrankJonesDtoPersisted() {
+        return DeveloperDto.builder()
+                .id(3)
+                .firstName("Frank")
+                .lastName("Jones")
+                .email("frank.jones@mail.com")
                 .specialty("Java")
                 .status(Status.DELETED)
                 .build();
